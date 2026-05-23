03bfb9ab34ee5c95a51457e1e4d4c8d14eb62e1b
konstahirvonen <konsta.hirvonen@live.fi>
2026-05-23 15:06:39 +0300

    Added check for whether user is signed in or not

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,30 @@
+"use client"
+
+import { supabase } from "@/lib/supabase"
+import { useRouter } from "next/navigation"
+import { useEffect, useState } from "react"
 
 
 export default function Home() {
+  const router = useRouter()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    supabase.auth.getUser().then(({ data: { user } }) => {
+      if (!user) {
+        router.push("/login")
+      } else {
+        setLoading(false)
+      }
+    })
+  }, [])
+
+  if (loading) return (
+    <div className="min-h-screen flex items-center justify-center">
+        <div className="w-8 h-8 border-4 border-black border-t-transparent rounded-full animate-spin" />
+    </div>
+  )
+
   return (
     <div className="min-h-screen flex items-center justify-center">
       <h1 className="text-4xl font-bold">In progress...</h1>
