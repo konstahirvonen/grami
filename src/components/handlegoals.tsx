f19985ad1a8cbe43602c5d93c868ab57d45b9bdf
konstahirvonen <konsta.hirvonen@live.fi>
2026-06-09 12:18:18 +0300

    Added button to save daily results

--- src/components/handlegoals.tsx
+++ src/components/handlegoals.tsx
@@ -60,7 +60,26 @@ export default function HandleGoals({ userId, totalCalories, setTotalCalories, t
           await handleGoals()
           setGoalsOpen(false)
     }
-    
+
+    const handleSaveDayResults = async () => {
+
+      const { error } = await supabase
+        .from("day_result")
+        .insert({
+          user_id: userId,
+          date: new Date().toISOString().split("T")[0],
+          calories: totalCalories,
+          protein: totalProtein,
+          carbs: totalCarbs,
+          fat: totalFat
+        })
+
+        if (error) {
+          console.log(error.message)
+        } else {
+          console.log("Tulokset tallennettu!")
+        }
+    }
 
     return (
 
@@ -93,6 +112,14 @@ export default function HandleGoals({ userId, totalCalories, setTotalCalories, t
             <button onClick={() => setGoalsOpen(true)} className="hover:bg-neutral-900 cursor-pointer rounded-xl p-1">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+              </svg>
+            </button>
+          </div>
+
+          <div className="absolute bottom-2 right-2">
+            <button onClick={handleSaveDayResults} className="border-1 border-[#404040] bg-[#10b981] hover:bg-[#0d9166] cursor-pointer rounded-xl p-1">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z" />
               </svg>
             </button>
           </div>
