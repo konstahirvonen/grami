10508873d1f0d3076d229dc432a069b0d2734938
konstahirvonen <konsta.hirvonen@live.fi>
2026-06-26 16:16:30 +0300

    camera fix n.2

--- next.config.ts
+++ next.config.ts
@@ -3,5 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
+module.exports = {
+  allowedDevOrigins: ['192.168.101.105'],
+}
 
 export default nextConfig;
